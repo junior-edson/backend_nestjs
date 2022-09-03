e1784f4a92341ccfb92338f6f3daa1d021f93321
Edson Junior <junior.edson@me.com>
2022-09-03 15:01:29 -0300

    Fix importing class

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { PlayersModule } from './players/players.module';
 import { AuthModule } from './auth/auth.module';
 import { SkillsModule } from './skills/skills.module';
 import { BattleModule } from './battle/battle.module';
-import { BattleZombieService } from './battle-zombie/battle-zombie.service';
+import { BattleZombieService } from './battle/battlezombie.service';
 
 @Module({
   imports: [PlayersModule, AuthModule, SkillsModule, BattleModule],
